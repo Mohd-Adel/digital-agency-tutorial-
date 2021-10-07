1f369ad0dd1d1afd6b89892543987d26bed2ba87
Mohd-Adel <mohamed.adel.ism@outlook.com>
2021-10-07 17:31:10 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// JavaScript Code
+//------------------search&close toggler----------------------------------------
+$(document).ready(function(){
+    //search toggle button
+    $(function(){
+        $(".toggle-overlay").click(function(){
+            $(".search-overlay").toggleClass("open");
+        })      
+    })
+})
+//------------------------------------------------------------------------------
+
+    
+    
+    
+    
+    
+    
+    
+    
